793dc1fad88a8dcb8837c3fe7bb0588873ded1f5
jjshawshaw <jshaw00@stuy.edu>
2019-03-16 19:58:21 -0400

    debugged mergesort now functioning

--- Merge.java
+++ Merge.java
@@ -6,46 +6,53 @@ public class Merge{
     mergesort(data, 0, data.length - 1);
   }
   private static void mergesort(int[] data, int start, int end){
-    int len = end - start;
-    int pivot = end + start / 2;
+    int len = end - start + 1;
+    int pivot = (end + start) / 2;
     if (len <= 1) return;
     if (len == 2){
-      if (data[start] > data[end]) swap(data, start, end);
+      if (data[start] > data[end]){
+        swap(data, start, end);
+      }
     }
     else{
-      int[] d1 = sublist(data, start, pivot - 1);
-      int[] d2 = sublist(data, pivot, end);
+      int[] d1 = Arrays.copyOfRange(data, start, pivot + 1);
+      int[] d2 = Arrays.copyOfRange(data, pivot + 1, end + 1);
       mergesort(d1);
       mergesort(d2);
-      data = merge(d1, d2);
+      int[] merged = merge(d1, d2);
+      for (int i = 0; i < data.length; i++){
+        data[i] = merged[i];
+      }
     }
   }
 
   private static int[] merge(int[] data1, int[] data2){
     int[] out = new int[data1.length + data2.length];
-    for (int i = 0; i < Math.min(data1.length, data2.length); i++){
-      out[i] = Math.min(data1[i], data2[i]);
-    }
-    if (data1.length > data2.length){
-      for (int i = data2.length; i < data1.length; i++){
-        out[i] = data1[i];
+    int i1 = 0;
+    int i2 = 0;
+    int iout = 0;
+    while (i1 < data1.length || i2 < data2.length) {
+      if (i1 >= data1.length){
+         out[iout] = data2[i2];
+         i2++;
+       }
+      else if (i2 >= data2.length){
+        out[iout] = data1[i1];
+        i1++;
       }
-    }
-    if (data2.length > data1.length){
-      for (int i = data1.length; i < data2.length; i++){
-        out[i] = data2[i];
+      else if (data1[i1] > data2[i2]){
+        out[iout] = data2[i2];
+        i2++;
       }
+      else {
+        out[iout] = data1[i1];
+        i1++;
+      }
+      iout++;
     }
     return out;
   }
 
-  private static int[] sublist(int[] data, int first, int last){
-    int[] temp = new int[last - first];
-    for (int i = first; i < last; i++){
-      temp[i] = data[i];
-    }
-    return temp;
-  }
 
   private static void swap(int[] data, int i1, int i2){
     int temp = data[i2];
@@ -54,7 +61,7 @@ public class Merge{
   }
 
   public static void main(String[]args){
-    int[] i = new int[]{4, 6, 8, 23, 2, 1, 0};
+    int[] i = new int[]{9, 6, 3, 23, 2, 1, 0};
     System.out.println(Arrays.toString(i));
     mergesort(i);
     System.out.println(Arrays.toString(i));
